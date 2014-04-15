97e36dd9c574a90cc65aa78e080d82447fedfe25
kristof1104 <kristof_50@hotmail.com>
2014-04-15 18:53:16 +0200

    new version

---
+++ CompetitorMod.js
@@ -0,0 +1,13 @@
+﻿(function () {
+	var ready = function () {
+		CompetitorMod.init();
+	};
+
+	var error = function () {
+	};
+
+	GDT.loadJs(
+	['mods/CompetitorMod/source/CompetitorSales.js',
+	'mods/CompetitorMod/source/CompetitorPublisher.js',
+	'mods/CompetitorMod/source/source.js'], ready, error);
+})();
